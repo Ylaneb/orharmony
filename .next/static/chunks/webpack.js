1a435a7a868c85c4537e9ed95511955297832f9c
Ylaneb <ylane.b@gmail.com>
2025-07-05 19:53:11 +0300

    Remove unused npm dependencies - clean up @dnd-kit, @tanstack/react-table, recharts, sonner, vaul, zod, @types/node, next-themes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ec1b390cb3bf881"; }
+/******/ 		__webpack_require__.h = function() { return "02f8e2400893a11c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
